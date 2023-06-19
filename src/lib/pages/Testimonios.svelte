02497fbf3c9b4bf53a6643ae7f2b728020df895e
factoria F5 <tatiyanakedrova94@gmail.com>
2023-06-19 11:15:22 +0200

    He creado section Testimpnios en la pagina principal

---
+++ src/lib/pages/Testimonios.svelte
@@ -0,0 +1,52 @@
+
+<div class="card-display">
+  <h1>Testimonios y reseñas</h1>
+  <div class="cards">
+  <div class="card-container">
+    <Card padded>" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "
+      <br><br>Martha Stewart
+     </Card>
+  </div>
+
+  <div class="card-container">
+    <Card>
+      <Content>" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "
+        <br> <br>Ariana Menage</Content>
+    </Card>
+  </div>
+
+  <div class="card-container">
+    <Card variant="outlined" padded>" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "
+      <br><br>Sean Carter</Card>
+  </div>
+</div>
+</div>
+
+
+
+<script lang="ts">
+  import Card, { Content } from '@smui/card';
+</script>
+
+<style>
+
+  .card-display{
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  
+    margin: 20px;
+  }
+
+  .cards{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  .card-container{
+    width: 250px;
+border: solid grey 1px;
+  }
+</style>
+
+
