fdfcbe9e1672e924d80efe6bf377d16de8803a67
jutelma <jutelma07051980@gmail.com>
2023-06-19 11:31:18 +0200

    Añadiendo cambios

---
+++ src/lib/components/Testimonios.svelte
@@ -0,0 +1,21 @@
+<script>
+    let testimonios = [
+        {
+            autor: 'Juan',
+            texto: '¡Excelente servicio! Recomiendo esta empresa a todos.',
+        },
+        {
+            autor: 'María',
+            texto: 'Increíble producto. Superó mis expectativas.',
+        },
+    ];
+</script>
+
+<div>
+    {#each testimonios as testimonio}
+        <div>
+            <h3>{testimonio.autor}</h3>
+            <p>{testimonio.texto}</p>
+        </div>
+    {/each}
+</div>
